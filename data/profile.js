d95c5213addadea3fb2f897b2f4df2b196f77cd5
eppoheemstra <97230878+eppoheemstra@users.noreply.github.com>
2022-01-06 14:04:55 +0100

    Update profile.js

--- data/profile.js
+++ data/profile.js
@@ -1,7 +1,7 @@
 const profileData = {
   title: 'Profile',
   name: 'Eppo Heemstra',
-  sub_title: 'Ethical Hacker',
+  sub_title: 'Hondje Deluxe',
   about: {
     intro: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
