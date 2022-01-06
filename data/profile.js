ed22263c10d35af14d7c352a5f6621fc3bff63a5
eppoheemstra <97230878+eppoheemstra@users.noreply.github.com>
2022-01-06 14:23:31 +0100

    Update profile.js

--- data/profile.js
+++ data/profile.js
@@ -6,7 +6,7 @@ const profileData = {
     intro: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
     contact: {
-      email: 'asdf@jkl.com',
+      email: 'eppio@pm.me',
       phone: '+1-1001010010',
       address: 'New York, USA'
     }
