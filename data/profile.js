eba03f61c0563acf2e6d4e360cd1cdaba7c34c37
eppoheemstra <97230878+eppoheemstra@users.noreply.github.com>
2022-01-06 14:02:42 +0100

    Update profile.js

--- data/profile.js
+++ data/profile.js
@@ -1,407 +1,103 @@
-{
-  "$schema": "https://raw.githubusercontent.com/jsonresume/resume-schema/v1.0.0/schema.json",
-  "basics": {
-    "name": "Eppo Heemstra",
-    "label": "Senior Digital Consultant at AkzoNobel",
-    "image": "",
-    "email": "epstra@outlook.com",
-    "phone": "",
-    "url": "",
-    "summary": "Digital marketing and IT specialist with international experience and track record of delivering value to organizations strategically and operationally. Excellent collaborator and communicator with strong stakeholder and project management skills combined with in-depth technical knowledge and a hands-on problem solving approach. Passionate about the combination of (marketing) technology (Web, Advertising, Data Analytics) and business operations. MarTech Allrounder, Website Product Owner and Certified Scrum Master.",
-    "location": {
-      "countryCode": "US",
-      "address": "Netherlands"
-    },
-    "profiles": [
-      {
-        "network": "LinkedIn",
-        "username": "eppoheemstra",
-        "url": "https://www.linkedin.com/in/eppoheemstra/"
-      }
-    ]
+const profileData = {
+  title: 'Profile',
+  name: 'Eppo Heemstra',
+  sub_title: 'Ethical Hacker',
+  about: {
+    intro: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    contact: {
+      email: 'asdf@jkl.com',
+      phone: '+1-1001010010',
+      address: 'New York, USA'
+    }
   },
-  "work": [
+  links: [
+    { title: 'Google', src: 'https://www.google.com/' },
+    { title: 'Facebook', src: 'https://www.facebook.com/' }
+  ],
+  education: [
     {
-      "name": "AkzoNobel",
-      "position": "Senior Consultant | Project Manager / Business Analyst",
-      "startDate": "2021-04-30",
-      "endDate": "",
-      "highlights": [],
-      "summary": "Temporary projects in digital transformation, web rollout management and business analyst consulting.\n\n- Manage and deliver Adobe AEM rollout projects end-to-end with a wide range of stakeholders and business units\n- Digital Marketing project management for AkzoNobel's largest yearly campaign\n\nContracted through IntoAction",
-      "url": "https://www.linkedin.com/company/akzonobel/",
-      "location": "Amsterdam, North Holland, Netherlands"
+      alma: 'Lorem Ipsum',
+      duration: '1978 - 1980',
+      std: 'MSC',
+      score: '99.99%'
     },
     {
-      "name": "Mambu",
-      "position": "Senior Manager, Marketing Operations",
-      "startDate": "2019-01-31",
-      "endDate": "2021-03-31",
-      "highlights": [],
-      "summary": "Product owner for mambu.com,  responsible for global digital marketing programs and MarTech platforms. Designed jamstack-based architecture and digital marketing applications in a B2B context. \nAgile software development: Product Owner and Servant Leader\nWeb & Marketing technology stack development\nDigital marketing operations\nData, reporting, analytics & automation\nMarketing Technology Management\nJAMstack web architecture development and implementation",
-      "url": "https://www.linkedin.com/company/mambu/",
-      "location": "Amsterdam, North Holland, Netherlands"
-    },
-    {
-      "name": "Ad Ultima Group transforms into 9altitudes",
-      "position": "Marketing Manager",
-      "startDate": "2018-02-28",
-      "endDate": "2019-01-31",
-      "highlights": [],
-      "summary": "Marketing Management\nMicrosoft Partner Marketing\nEvents Management\nWordpress Development\n\nOrganized and hosted Pylades' yearly customer-focus event\nTransformed the web presence with Wordpress web development agency\nSet up and deployed omnichannel digital marketing operations: Search, display advertising, and measurement approach. Implemented lead-gen operations support for sales.",
-      "url": "https://www.linkedin.com/company/9altitudes/",
-      "location": "Amsterdam, North Holland, Netherlands"
-    },
-    {
-      "name": "Axon",
-      "position": "Digital Marketing Manager EMEA",
-      "startDate": "2016-11-30",
-      "endDate": "2017-12-31",
-      "highlights": [],
-      "summary": "Digital Marketing\nWeb Management\nData & Reporting and Email Marketing campaigns\nRebranding\nLocalisation & Translation projects\nDemand gen program setup\nMarketing technology setup",
-      "url": "https://www.linkedin.com/company/axon-protect-life/",
-      "location": "Amsterdam, North Holland, Netherlands"
-    },
-    {
-      "name": "G Data Software",
-      "position": "Global Digital Marketing Manager",
-      "startDate": "2014-02-28",
-      "endDate": "2016-08-31",
-      "highlights": [],
-      "summary": "G Data is a German software company that focuses on IT security. The company was founded in 1985 and is headquartered in Bochum.\n\nThey are known for being the creators of the world's first antivirus software.\n\nB2C Digital Marketing\nE-commerce & ROI reporting\nProgrammatic buying advertising programs\nDigital Marketing\nMeasurement programs: analytics & reporting\nSales enablement\nCopywriting\nCreative concept production\nTranslation & localisation",
-      "url": "https://www.linkedin.com/company/g-data-software/",
-      "location": "Bochum, North Rhine-Westphalia, Germany"
-    },
-    {
-      "name": "Insight",
-      "position": "E-Marketing Specialist",
-      "startDate": "2011-06-30",
-      "endDate": "2013-12-31",
-      "highlights": [],
-      "summary": "Web management\nE-commerce projects\nB2B digital marketing\nContent marketing\nAnalytics & reporting\nEmail marketing\nTranslation & Localisation",
-      "url": "https://www.linkedin.com/company/insight/",
-      "location": "Sheffield, England, United Kingdom"
-    },
-    {
-      "name": "Gorkana",
-      "position": "Researcher",
-      "startDate": "2010-09-30",
-      "endDate": "2011-06-30",
-      "highlights": [],
-      "summary": "Media research\nDatabase administration\nInterviewing\nCopywriting",
-      "url": "https://www.linkedin.com/company/cisionuk/",
-      "location": "London, England, United Kingdom"
+      alma: 'Dotor Sit Amet',
+      duration: '1975 - 1977',
+      std: 'SomeThing',
+      score: '99.99%'
     }
   ],
-  "volunteer": [
+  skills: [
     {
-      "organization": "nTitle",
-      "position": "Advisor",
-      "startDate": "2017-12-31",
-      "endDate": "2018-12-31",
-      "summary": "Blockchain side project: digital marketing consultancy",
-      "highlights": [],
-      "url": "https://www.linkedin.com/company/18425189"
+      category: 'Lorem Ipsum',
+      topics: ['Dolor', 'Sit', 'Amet', 'Consectetur', 'Adipiscing', 'Elit']
+    },
+    {
+      category: 'Sed / Eiusmod',
+      topics: [
+        'Tempor',
+        'Incididunt',
+        'Labore',
+        'Dolore',
+        'Magna',
+        'Aliqua',
+        'Labore'
+      ]
+    },
+    {
+      category: 'Minim Veniam',
+      topics: ['Quis', 'Nostrud', 'Exercitation', 'Ullamco']
     }
   ],
-  "education": [
+  projects: [
     {
-      "institution": "Hanzehogeschool Groningen / Hanze University of Applied Sciences ",
-      "area": "Information & Communication Management",
-      "studyType": "Bachelor's degree",
-      "startDate": "2005-12-31",
-      "endDate": "2009-12-31",
-      "score": "",
-      "courses": []
+      title: 'Lorem Ipsum',
+      duration: 'Mar - Nov 1885',
+      link: 'https://github.com/',
+      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`
     },
     {
-      "institution": "Alfa-college",
-      "area": "Law",
-      "studyType": "Higher vocational degree",
-      "startDate": "2002-12-31",
-      "endDate": "2005-12-31",
-      "score": "",
-      "courses": []
+      title: 'Lorem Ipsum',
+      duration: 'Apr - Jul 1917',
+      link: 'https://github.com/',
+      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`
     }
   ],
-  "awards": [],
-  "certificates": [
+  experiences: [
     {
-      "name": "Certified ScrumMaster (CSM)",
-      "issuer": "Scrum Alliance",
-      "endDate": "2022-02-28",
-      "startDate": "2020-02-28"
+      organization: 'World War 2',
+      title: `General`,
+      date: '1941 - 1945',
+      details: [
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+      ]
+    },
+    {
+      organization: 'World War 1',
+      title: `Soldier`,
+      date: '1914 - 1918',
+      details: [
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+        `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+      ]
     }
   ],
-  "publications": [],
-  "skills": [
+  certifications: [
     {
-      "name": "E-commerce",
-      "level": "",
-      "keywords": []
+      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      date: 'Aug 1917'
     },
     {
-      "name": "SEO",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Copywriting",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Translation",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "G DATA Software",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "docker",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Web Hosting",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Debian",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Communication",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Strategy",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Adobe Creative Suite",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Reasoning",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Content Management",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Office 365",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Branding & Identity",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Agile Methodologies",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Content Management Systems (CMS)",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Negotiation",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Google Ads",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "negotiation",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "English",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Contract Negotiation",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Search Advertising",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Linux",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Facilitation",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Interdisciplinary Collaboration",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "LAMPstack",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Web Analytics",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "DNS",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Conversion Optimization",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Docker",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Networking",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Analytical Skills",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Display Advertising",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Google Analytics",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "ZFS",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Data Analysis",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Dutch",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Ubuntu Server",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Analytics",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Research",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "Problem Solving",
-      "level": "",
-      "keywords": []
-    },
-    {
-      "name": "JAMstack",
-      "level": "",
-      "keywords": []
+      desc: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      date: 'Jul 1945'
     }
   ],
-  "languages": [
-    {
-      "fluency": "Native Speaker",
-      "language": "Dutch"
-    },
-    {
-      "fluency": "Native Speaker",
-      "language": "English"
-    },
-    {
-      "fluency": "Professional Working",
-      "language": "German"
-    }
-  ],
-  "interests": [],
-  "references": [
-    {
-      "name": "Martine Korstanje (MBA)",
-      "reference": "“Eppo is a very proactive Digital Marketing manager with a strong customer focus. He has developed and managed the digital marketing strategy for Axon in EMEA, setting a solid basis for international growth. He is able to oversee global digital marketing campaigns from the program management process to the campaign execution. He also has excellent communication skills and provides clear directions  to other members of the Marketing Team. Eppo has implemented successfully a SEO/SEA strategy and managed the localization process for the international websites. Eppo has a great sense of humour and it’s a pleasure to work with him.”"
-    },
-    {
-      "name": "Andreas Schneble",
-      "reference": "Eppo managed several key programs as Global Digital Marketing Manager on my team, focused on online customer acquisition and ecommerce. He has a broad understanding of all elements of digital marketing and constantly remains up to date on the latest platforms and trends. I very much appreciated his willingness to take on new initiatives, and his ability to bring together the various stakeholders needed for a successful project. Eppo would be a great addition to any digital marketing or ecommerce team."
-    },
-    {
-      "name": "Andy Van Spijkeren",
-      "reference": "I managed Eppo Heemstra for more than a year, during that time he performed exceptionally on tasks and projects. He is a multi-skilled professional with excellent communication skills, reliable and diligent in his work - Eppo Heemstra's ideas, skills, knowledge and understanding of Online Marketing allowed him to be very proficient in his day to day tasks and overall projects. Eventually he moved to the role of E-Commerce Team lead to become a manger of people himself and running strong and successful marketing campaigns that had a strong impact according KPIs set by Insight: Content, traffic, e-mail open/click rates, cost per lead and overall brand awareness."
-    },
-    {
-      "name": "Tobias Del Fa",
-      "reference": "Eppo and I worked together during the early stages of Mediaplanet's online platform launch and collaborated on the foundations of this strategy. Some of his suggestions were essential for the improvement of our digital product and its performance. Eppo is a great team player and a creative thinker who works very well in fast paced environments. "
-    },
-    {
-      "name": "Patrick Stoltze",
-      "reference": "I have been working together with Eppo in order to establish a programmatic media buying partnership for G DATA in 2015. It has not only been a pleasure to work with Eppo on the personal level but also from the professional side to make this project grow month by month. Following constructive negotiations he has always been able to let us know which areas to improve in order to most efficiently make use of the programmatic technologies. \r\n\r\nSummarized: Bright guy with excellent RTA knowledge and most importantly also reliable professional behavior. Pleasure to be working with you. "
-    },
-    {
-      "name": "Tim Andrews",
-      "reference": "Since most of my work is within web design and programming, Eppo can best be described as my 'partner-in-crime' here at MediaPlanet. We have worked alongside various projects from small to large and it has been a very productive, enjoyable and positive experience.\r\n\r\nEppo has extensive knowledge in SEO, SEM, social media marketing and email marketing. He is also very capable of thinking within tight deadlines, small budgets and restricted options. Overall, he has impressed me with his knowledge, determination and work ethic.\r\n\r\nI can also commend Eppo on his personality. He is friendly, easy to work with, has a great sense of humor and most importantly, has a huge passion and interest in his career and the company he works for. It has been very refreshing to work alongside someone with such an investment in improving and challenging the company and the people around him. He is also very skilled in translating his job and all the technical aspects of it into terms that anyone can understand."
-    },
-    {
-      "name": "Fleur van den Horn",
-      "reference": "I have hired Eppo Heemstra while working with an international company seeking a translator and press writer for bth Dutch and English. In 2012 I have asked Eppo to translate my personal website and CV. Eppo is an easy to work with, reliable, friendly person to work with. Delivering on his promises and in time. Highly recommendable."
-    },
-    {
-      "name": "Michael Webster",
-      "reference": "Eppo was a key member of our European team.  He is enthusastic, hard working and full of ideas.   A pleasure to have in the team."
-    }
-  ],
-  "projects": [],
-  "meta": {
-    "version": "v1.0.0",
-    "canonical": "https://github.com/jsonresume/resume-schema/blob/v1.0.0/schema.json"
-  }
-}
+  events: []
+};
